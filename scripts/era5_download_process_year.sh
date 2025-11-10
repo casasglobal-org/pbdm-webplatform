4d8e005977f1f1e382b27ec238cc0488a0bf4e10
Luca Delucchi <lucadeluge@gmail.com>
2025-11-10 08:55:18 +0100

    added script to download and process one year of era data

---
+++ scripts/era5_download_process_year.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+YEAR=$1
+
+if [ -z "$YEAR" ]; then
+    echo "Usage: $0 <YEAR>"
+    exit 1
+fi
+
+this_dir=`dirname $0`
+main_dir=`dirname $this_dir`
+env_dir="$main_dir/venv"
+
+if [ ! -d "$main_dir" ]; then
+    echo "Main directory does not exist"
+fi
+
+if [ ! -d "$env_dir" ]; then
+    echo "Virtual environment directory does not exist"
+else
+    source "$env_dir/bin/activate"
+fi
+
+env_dir="$main_dir/.env"
+
+if [ ! -d "$env_dir" ]; then
+    echo "Environment directory does not exist"
+    exit 1
+else
+    source "$env_dir/bin/activate"
+fi
+
+env_dir="$main_dir/.venv"
+
+if [ ! -d "$env_dir" ]; then
+    echo "Virtual environment directory does not exist"
+    exit 1
+else
+    source "$env_dir/bin/activate"
+fi
+
+env_dir="$main_dir/.env"
+
+if [ ! -d "$env_dir" ]; then
+    echo "Environment directory does not exist"
+    exit 1
+else
+    source "$env_dir/bin/activate"
+fi
+
+time python ${main_dir}/casas_web_portal/manage.py download_era5 $YEAR
+
+time python ${main_dir}/casas_web_portal/manage.py create_txt_era5 --dir /opt/casas_data/ $YEAR
+
+rm -f /opt/casas_data/data/era5/*/*-$YEAR-*-merged.nc
+
+exit
