e0a6e3bc636c8ae6f5a06209036171e6d7d32acc
Luca Delucchi <lucadeluge@gmail.com>
2026-01-25 06:45:19 +0100

    starting code for running command

--- casas_web_portal/webapp/models.py
+++ casas_web_portal/webapp/models.py
@@ -1,8 +1,18 @@
+import os
+import glob
+import geopandas as gpd
+import subprocess
+from django.conf import settings
 from django.contrib.gis.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.encoding import smart_str
 from django.utils.translation import gettext_lazy as _
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from huey.contrib import djhuey as huey
+from casas_web_portal.functions import _send_mail
+
+# Create your models -------here.
 
 CONTINENT = (
     ("EU", _("Europe")),
@@ -13,15 +23,9 @@ CONTINENT = (
     ("OC", _("Oceania")),
 )
 
-PDBMS = (
-    ("olive", _("Olive")),
-    ("another", _("Another"))
-)
+PDBMS = (("olive", _("Olive")), ("another", _("Another")))
 
-WEATHERS = (
-    ("era5", "AgERA5 v2.0"),
-    ("cmip6", "NEX-GDDP-CMIP6 v2.0")
-)
+WEATHERS = (("era5", "AgERA5 v2.0"), ("cmip6", "NEX-GDDP-CMIP6 v2.0"))
 
 
 class BaseDigi(models.Model):
@@ -52,9 +56,7 @@ class Country(BaseDigi):
 
     shortname = models.CharField(max_length=3)
     geom = models.MultiPolygonField(srid=4326, geography=True)
-    continent = models.CharField(
-        max_length=2, choices=CONTINENT, null=True, blank=True
-    )
+    continent = models.CharField(max_length=2, choices=CONTINENT, null=True, blank=True)
 
     class Meta(BaseDigi.Meta):
         db_table = "general_country"
@@ -115,7 +117,7 @@ class User(AbstractUser):
         Organization, on_delete=models.PROTECT, null=True, blank=True
     )
 
-    class Meta():
+    class Meta:
         db_table = "general_user"
 
     @property
@@ -164,3 +166,72 @@ class Job(BaseDigi):
     def __str__(self):
         output = f"{self.name} {self.model} {self.weather} - {self.start_date}/{self.end_date}"
         return smart_str(output)
+
+
+@huey.task()
+def run_pbdm_script(job_id):
+    """Function to run the PBDM script for a given job
+
+    Args:
+        job_id (int): The job id
+    """
+    # Here you would add the code to run the PBDM script
+    print(f"Running PBDM script for job {job_id}")
+    job = Job.objects.get(id=job_id)
+    job.started = models.DateTimeField(auto_now=True)
+    job.save()
+    _send_mail(
+        "Job Started",
+        f"Your job has started, you will receive an email when it will be finished.",
+        job.user,
+    )
+    filepath = os.path.join(
+        settings.STATIC_ROOT, "data", "{}_grid_land.fgb".format(job.weather)
+    )
+    points_gdf = gpd.read_file(filepath)
+    points_within = points_gdf[points_gdf.geometry.within(job.geom)]
+    txt_dir = os.path.join(settings.TXT_DIR, job.weather)
+    files = []
+    errors = []
+    for point in points_within.itertuples():
+        infiles = glob.glob(
+            os.path.join(txt_dir, f"*_{point.attrs.col}_{point.attrs.row}.txt.gz")
+        )
+        files.extend(infiles)
+        for infile in infiles:
+            print(f"Processing file {infile} for job {job_id}")
+            try:
+                out = subprocess.run([], check=True, shell=True)
+            except Exception as e:
+                print(f"Error processing file {infile} for job {job_id}: {e}")
+                errors.append((infile, str(e)))
+            print(f"Finished processing file {infile} for job {job_id}")
+            # Process the file (this is just a placeholder)
+    job.ended = models.DateTimeField(auto_now=True)
+    job.save()
+    if len(errors) > 0:
+        error_messages = "\n".join([f"{f}: {msg}" for f, msg in errors])
+        _send_mail(
+            "Job Completed with Errors",
+            f"Your job with ID {job.id} has been completed but some errors occurred:\n{error_messages}\nYou can now check the maps selecting the variables of your interest on the following link https://{os.environ.get("APP_URL")}/job/{job.id}/",
+            job.user,
+        )
+    else:
+        _send_mail(
+            "Job Completed",
+            f"Your job has been completed. You can now create the maps selecting the variables of your interest on the following link https://{os.environ.get("APP_URL")}/job/{job.id}/",
+            job.user,
+        )
+
+
+@receiver(post_save, sender=Job)
+def create_run_analisys(sender, instance, created, **kwargs):
+    """Signal to create the run analysis when a new job is created
+
+    Args:
+        sender (_type_): _description_
+        instance (_type_): _description_
+        created (_type_): _description_
+    """
+    if created:
+        run_pbdm_script(instance.id)
